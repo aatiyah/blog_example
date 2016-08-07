627e10f377bf52d10cc9a9adcee3608cf29a7151
aatiyah <medatiyah@gmail.com>
2016-08-06 21:34:44 -0500

    Added modal for devise login/sign up/edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,4 @@ $(function() {
     $(this).addClass("active"); 
   });
 });
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,16 @@ module ApplicationHelper
 
 		Redcarpet::Markdown.new(renderer, options).render(text).html_safe
 	end
+
+	def resource_name
+		:user
+	end
+
+	def resource
+		@resource ||= User.new
+	end
+
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+	end
 end
