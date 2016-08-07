6cceeba7faba1ba67e5b4ea258573f9fa001f83c
aatiyah <medatiyah@gmail.com>
2016-08-07 17:03:20 -0500

    Added config action mailer for Heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,11 +11,6 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-
-    respond_to do |format|
-      format.html
-      format.atom
-    end
   end
 
   def show
